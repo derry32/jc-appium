62216fb6fe82fc8a033b89cb03aa2725e9951624
derry32 <derry32@gmail.com>
2024-08-24 07:23:54 +0700

    JC-Appium

---
+++ src/main/java/com/juaracoding/FirstAppium.java
@@ -0,0 +1,34 @@
+package com.juaracoding;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class FirstAppium {
+    public static void main(String[] args) throws MalformedURLException {
+        DesiredCapabilities dc = new DesiredCapabilities();
+        dc.setCapability("deviceName", "Pixel 2 API 30");
+        dc.setCapability("udid", "emulator-5554");
+        dc.setCapability("platformName", "android");
+        dc.setCapability("platformVersion", "11");
+        dc.setCapability("appPackage", "com.google.android.calculator");
+        dc.setCapability("appActivity", "com.android.calculator2.Calculator");
+        dc.setCapability("noReset", true);
+        URL url = new URL("http//127.0.0.1:4723/wd/hub");
+        AndroidDriver driver = new AndroidDriver(url, dc);
+
+        MobileElement btnOne = (MobileElement) driver.findElementById("com.google.android.calculator:id/digit_1");
+        btnOne.click();
+        MobileElement btnPlus = (MobileElement) driver.findElementById("com.google.android.calculator:id/op_add");
+        btnPlus.click();
+        MobileElement btnTwo = (MobileElement) driver.findElementById("com.google.android.calculator:id/digit_2");
+        btnTwo.click();
+        MobileElement btnEquals = (MobileElement) driver.findElementById("com.google.android.calculator:id/eq");
+        btnEquals.click();
+        MobileElement resultFinal = (MobileElement) driver.findElementById("com.google.android.calculator:id/result_final");
+        System.out.println(resultFinal.getText());
+    }
+}
